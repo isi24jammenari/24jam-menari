56bce88cc51c8dd8b0671e729fef4c5aeeb8ec2e
TicketGo <ticketgo.ticketing.id@gmail.com>
2026-04-03 01:37:58 +0700

    finalisasi

--- src/middleware.ts
+++ src/middleware.ts
@@ -55,5 +55,7 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   // Terapkan ke semua rute kecuali file statis dan API Next.js internal
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 };
